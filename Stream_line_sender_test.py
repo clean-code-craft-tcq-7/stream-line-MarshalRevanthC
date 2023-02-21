f903d533e5fd5b47dc0e2f1171395779874439cc
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2023-02-21 08:18:40 +0530

    Create Stream_line_sender_test.py
    stream line Sender Test code

---
+++ Stream_line_sender_test.py
@@ -0,0 +1,13 @@
+""""""""""""" Test Code """"""""""""""
+
+import unittest
+
+def run_tests():
+    loader = unittest.TestLoader()
+    suite = loader.discover(start_dir="test_code", pattern="t_*.py")
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite)
+    return result
+
+if __name__ == "__main__":
+    run_tests()
